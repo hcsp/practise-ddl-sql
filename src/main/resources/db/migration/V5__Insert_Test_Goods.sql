2d43984ccfbd464d824076a624e0f890aa1f2b3a
uuflyer <1315684099@qq.com>
2019-09-01 23:42:36 +0800

    粗心大意

--- src/main/resources/db/migration/V5__Insert_Test_Goods.sql
+++ src/main/resources/db/migration/V5__Insert_Test_Goods.sql
@@ -1,4 +1,11 @@
-insert into GOODS (ID,NAME ,PRICE, LEFT, CREATED_AT, UPDATED_AT, STATUS) VALUES (200,'TEST_GOODS',123.00,now(),now(),1)
--- 请在这里编写一条SQL语句，在GOODS表中插入一条数据 -- ID           200 -- NAME         'TEST_GOODS' -- PRICE        123.00 -- LEFT         1 -- CREATED_AT   NOW()
+insert into ORDER (ID,NAME ,PRICE ,LEFT, CREATED_AT, UPDATED_AT, STATUS)
+VALUES (200,'TEST_GOODS',123.00,1,now(),now(),1)
+
+-- 请在这里编写一条SQL语句，在GOODS表中插入一条数据
+-- ID           200
+-- NAME         'TEST_GOODS'
+-- PRICE        123.00
+-- LEFT         1
+-- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
