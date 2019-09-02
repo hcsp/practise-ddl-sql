6ec4372b342853f1d771b9517f4b403cd366f007
kungua <52815012+kungua@users.noreply.github.com>
2019-09-02 17:30:03 +0800

    practise ddl sql (#14)
    practise ddl sql (#14)

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,16 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+
+create table USER
+(
+    ID         BIGINT primary key AUTO_INCREMENT,
+    NAME       varchar(100) NOT NULL,
+    TEL        varchar(20) not null unique,
+    ADDRESS    varchar(100),
+    CREATED_AT timestamp not null,
+    UPDATED_AT timestamp not null,
+    STATUS     tinyint not null
+)
+
+
