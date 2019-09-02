882c8973be0c7d809109bea06eb0fb7cbfb06842
kungua <maguahongkun@outlook.com>
2019-09-02 17:29:37 +0800

    revert test file change

--- src/test/java/com/github/hcsp/sql/SqlTest.java
+++ src/test/java/com/github/hcsp/sql/SqlTest.java
@@ -5,7 +5,6 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
-
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
