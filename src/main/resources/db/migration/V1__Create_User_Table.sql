341162e3e9d94537109469fb1b773fde389c342e
XiongAmao <onycat@live.cn>
2019-09-08 22:09:48 +0800

    Practise DDL SQL.

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,14 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table USER
+(
+    id         bigint primary key auto_increment,
+    name       varchar(100) not null comment '用户名',
+    tel        varchar(20)  not null comment 'phone', -- 这行如果写成 tel varchar(20) not null unique comment '电话'，无法通过测试，会报语法错误
+    address    varchar(100) comment '地址',
+    created_at timestamp    not null comment '创建时间',
+    updated_at timestamp    not null comment '修改时间',
+    status     tinyint      not null comment '用户状态: 1正常 -1删除',
+    unique (tel)
+);
