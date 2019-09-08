aa0fdba03a119329b3c1e296b2e28e1b0159507a
XiongAmao <onycat@live.cn>
2019-09-08 22:11:33 +0800

    Practise DDL SQL. (#20)
    Practise DDL SQL. (#20)

--- src/main/resources/db/migration/V2__Create_Goods_Table.sql
+++ src/main/resources/db/migration/V2__Create_Goods_Table.sql
@@ -6,3 +6,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table goods
+(
+    id         bigint primary key auto_increment,
+    name       varchar(100) not null comment '商品名',
+    price      decimal      not null comment '价格',
+    left       int          not null default 0 comment '库存，默认:0',
+    created_at timestamp    not null comment '创建时间',
+    updated_at timestamp    not null comment '修改时间',
+    status     tinyint      not null default 1 comment '商品状态: 1正常 -1删除'
+);
