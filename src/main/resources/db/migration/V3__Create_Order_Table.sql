51ece2d124710477c9f2b605537baeb055c8eb1d
emmmm <3108775859@qq.com>
2019-12-08 19:18:01 +0800

    crud

--- src/main/resources/db/migration/V3__Create_Order_Table.sql
+++ src/main/resources/db/migration/V3__Create_Order_Table.sql
@@ -7,7 +7,7 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
-create table 订单(
+create table `order`(
 id bigint primary key auto_increment,
 user_id bigint not null,
 goods_id bigint not null,
--- src/main/resources/db/migration/V6__Insert_Test_Order.sql
+++ src/main/resources/db/migration/V6__Insert_Test_Order.sql
@@ -7,5 +7,8 @@
 -- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
-insert into 'order'
+insert into `order`
+values (1,100,200,1,123.00,now(),now(),1)
+
+insert into `order`
 values (1,100,200,1,123.00,now(),now(),1)
