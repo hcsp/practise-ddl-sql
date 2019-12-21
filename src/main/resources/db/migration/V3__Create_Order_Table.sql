d01af06621cac09b35423b43c4ccd6a1094153a6
87244099 <872440899@qq.com>
2019-12-21 13:36:08 +0800

    执行sql

--- src/main/resources/db/migration/V3__Create_Order_Table.sql
+++ src/main/resources/db/migration/V3__Create_Order_Table.sql
@@ -7,3 +7,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+CREATE TABLE `ORDER`(
+    ID BIGINT PRIMARY KEY AUTO_INCREMENT, --ID
+    USER_ID BIGINT NOT NULL,--用户ID 不可为空
+    GOODS_ID BIGINT NOT NULL,--商品ID 不可为空
+    GOODS_NUM INT DEFAULT 0 NOT NULL,--商品数量 不可为空
+    GOODS_PRICE INT DEFAULT 0 NOT NULL,--商品单价 不可为空
+    CREATED_AT TIMESTAMP NOT NULL,--创建时间 不可为空
+    UPDATED_AT TIMESTAMP NOT NULL,--修改时间 不可为空
+    STATUS TINYINT NOT NULL--状态，1正常，-1删除  不可为空
+)
