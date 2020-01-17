79d9c94bd28ee7da987e077b02838161467a4e94
xdcgh <44773625+xdcgh@users.noreply.github.com>
2020-01-17 20:38:42 +0800

    完成数据库实战：表结构设计、建表语句、insert/update语句练习 (#58)
    完成数据库实战：表结构设计、建表语句、insert/update语句练习 (#58)

--- src/main/resources/db/migration/V4__Insert_Test_User.sql
+++ src/main/resources/db/migration/V4__Insert_Test_User.sql
@@ -6,3 +6,4 @@
 -- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
+insert into user (ID, NAME, TEL, ADDRESS, CREATED_AT, UPDATED_AT, STATUS) values (100, 'TEST_USER', '1234567890', NULL, NOW(), NOW(), 1 );
