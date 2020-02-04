798b88441a8ca86168419185d7a07e2ae4bc1b23
JN0916 <56193588+JN0916@users.noreply.github.com>
2020-02-04 15:58:24 +0800

    Update src/main/resources/db/migration/V2__Create_Goods_Table.sql
    Co-Authored-By: Bo Zhang <bo@gradle.com>

--- src/main/resources/db/migration/V2__Create_Goods_Table.sql
+++ src/main/resources/db/migration/V2__Create_Goods_Table.sql
@@ -11,7 +11,7 @@ create table goods
 id           bigint       primary key auto_increment,
 name         varchar(100) not null,
 price        decimal      not null,
-'left'      int          not null default 0,
+`left`      int          not null default 0,
 created_at   timestamp    not null,
 updated_at   timestamp    not null,
 status       tinyint      not null
