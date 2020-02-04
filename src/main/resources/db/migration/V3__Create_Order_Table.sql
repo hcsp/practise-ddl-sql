0c7ed8d1057850ea314031b7864710f9ceb2b374
JN0916 <56193588+JN0916@users.noreply.github.com>
2020-02-04 16:20:36 +0800

    sql语句练习 (#69)
    sql语句练习 (#69)

--- src/main/resources/db/migration/V3__Create_Order_Table.sql
+++ src/main/resources/db/migration/V3__Create_Order_Table.sql
@@ -7,3 +7,14 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table `ORDER`
+(
+id          bigint primary key auto_increment,
+user_id     bigint not null,
+goods_id    bigint not null,
+goods_num   int not null,
+goods_price decimal not null,
+created_at  timestamp not null,
+updated_at  timestamp not null,
+status      tinyint not null
+)
