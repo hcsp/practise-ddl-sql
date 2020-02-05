e63c13766df7e6f88827f797da80366ee2eb7b91
yaohengfeng <1065435454@qq.com>
2020-02-05 19:42:00 +0800

    sql语句测试 (#70)
    sql语句测试 (#70)

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table `User`
+(
+    ID         BIGINT primary key Auto_INCREMENt,
+    NAME       varchar(100) not null,
+    TEL        varchar(100) not null UNIQUE,
+    ADDRESS    varchar(100),
+    CREATED_AT TIMESTAMP    not null,
+    UPDATED_AT TIMESTAMP    not null,
+    STATUS     TINYINT      not null
+)
