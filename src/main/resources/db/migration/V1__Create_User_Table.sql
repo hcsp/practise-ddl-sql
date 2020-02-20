576a00ff1c8a53549603f09bd7aee8b255a58b39
silvia-YQY <545067444@qq.com>
2020-02-20 22:43:47 +0800

    表结构设计、建表语句、insert/update语句练习 (#73)
    表结构设计、建表语句、insert/update语句练习 (#73)

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table user
+(
+    ID bigint primary key AUTO_INCREMENT,
+    name varchar(100) not null,
+    tel varchar(20) not null unique,
+    address varchar(100),
+    CREATED_AT   TIMESTAMP not null,
+    UPDATED_AT   TIMESTAMP not null,
+    STATUS tinyint not null
+)
