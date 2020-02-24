6dba90a5d43b9b2fc46a69f729b0bf279a65ac69
Dygalo <59307187+dygalo@users.noreply.github.com>
2020-02-24 09:30:53 +0800

    practice ddl

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -16,11 +16,3 @@ create table user
     updated_at timestamp    not null,
     status     tinyint      not null
 )
-# create table USER( id bigint primary key auto_increment,
-# name varchar(100) not null,
-# tel varchar(20) ,
-# address varchar(100),
-# created_at timestamp not null,
-# updated_at timestamp not null,
-# status tinyint not null
-# )
