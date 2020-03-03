fb4c7ac747a34a4c98bc1358f35200fecfabeefc
maxiaoda <48616409+maxiaoda@users.noreply.github.com>
2020-03-03 20:20:15 +0800

    Add files via upload
    数据库实战：表结构设计、建表语句、insert/update语句练习

---
+++ V4__Insert_Test_User.sql
@@ -0,0 +1,8 @@
+-- 请在这里编写一条SQL语句，向USER插入一条测试数据
+-- ID           100
+-- NAME         'TEST_USER'
+-- TEL          '1234567890'
+-- ADDRESS      NULL
+-- CREATED_AT   NOW()
+-- UPDATED_AT   NOW()
+-- STATUS       1
---
+++ V6__Insert_Test_Order.sql
@@ -0,0 +1,9 @@
+-- 请在这里编写一条SQL语句，插入一条ORDER
+-- ID           1
+-- USER_ID      100
+-- GOODS_ID     200
+-- GOODS_NUM    1
+-- GOODS_PRICE  123.00
+-- CREATED_AT   NOW()
+-- UPDATED_AT   NOW()
+-- STATUS       1
