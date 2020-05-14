e5d973e90813110196aea84e5bf6088c69144c9b
DavidWildeTMP <64569468+DavidWildeTMP@users.noreply.github.com>
2020-05-14 11:02:06 -0700

    表结构设计、建表语句、insert/update语句练习 (#106)
    表结构设计、建表语句、insert/update语句练习 (#106)

--- src/main/resources/db/migration/V5__Insert_Test_Goods.sql
+++ src/main/resources/db/migration/V5__Insert_Test_Goods.sql
@@ -6,3 +6,5 @@
 -- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
+insert into goods (id, name, price, "LEFT", created_at, updated_at, status)
+values (200, 'TEST_GOODS', 123.00, 1, now(), now(), 1)
