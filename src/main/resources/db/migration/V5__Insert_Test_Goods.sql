f5a9ba3278958603658545a0ea42c85bd51e0a92
concise <15521608275@163.com>
2020-06-02 12:54:57 +0800

    try 4

--- src/main/resources/db/migration/V5__Insert_Test_Goods.sql
+++ src/main/resources/db/migration/V5__Insert_Test_Goods.sql
@@ -1,7 +1,7 @@
 INSERT INTO GOODS
     (ID,NAME,PRICE,LEFT,CREATED_AT,UPDATED_AT,STATUS)
         VALUES
-            (200,'TEST_GOODS',123.00,1, NOW(), NOW(), 1)
+           (200,'TEST_GOODS',123.00,1, NOW(), NOW(), 1)
 -- 请在这里编写一条SQL语句，在GOODS表中插入一条数据
 -- ID           200
 -- NAME         'TEST_GOODS'
