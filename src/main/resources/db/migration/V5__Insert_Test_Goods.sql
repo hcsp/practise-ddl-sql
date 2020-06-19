fcf0a50c5cb52fa0ec6a7e6461f227e32991a661
McDul <907420318@qq.com>
2020-06-19 15:55:55 +0800

    SQL语言增删改实战 (#116)
    SQL语言增删改实战 (#116)

--- src/main/resources/db/migration/V5__Insert_Test_Goods.sql
+++ src/main/resources/db/migration/V5__Insert_Test_Goods.sql
@@ -1,4 +1,5 @@
 -- 请在这里编写一条SQL语句，在GOODS表中插入一条数据
+INSERT INTO GOODS VALUES (200, 'TEST_GOODS', 123.00, 1, NOW(), NOW(), 1);
 -- ID           200
 -- NAME         'TEST_GOODS'
 -- PRICE        123.00
--- src/main/resources/db/migration/V6__Insert_Test_Order.sql
+++ src/main/resources/db/migration/V6__Insert_Test_Order.sql
@@ -1,4 +1,5 @@
 -- 请在这里编写一条SQL语句，插入一条ORDER
+INSERT INTO `ORDER` VALUES (1, 100, 200, 1, 123.00, NOW(), NOW(), 1)
 -- ID           1
 -- USER_ID      100
 -- GOODS_ID     200
