3eaf6a98e9718d7583a804a43528aa4ff6ecbe28
iRuby <1479867360@qq.com>
2020-12-19 13:14:10 +0800

    insert/update语句练习

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,35 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+
+create table USER
+(
+    ID bigint auto_increment,
+    NAME varchar(100) not null,
+    TEL VARCHAR(20) not null,
+    ADDRESS varchar(100),
+    CREATED_AT timestamp not null,
+    UPDATE_AT timestamp not null,
+    STATUS TINYINT not null,
+    constraint USER_PK
+        primary key (ID)
+);
+
+create unique index USER_TEL_UINDEX
+	on USER (TEL);
+
+comment on column USER.ID is 'ID';
+
+comment on column USER.NAME is '用户名';
+
+comment on column USER.TEL is '电话';
+
+comment on column USER.ADDRESS is '地址';
+
+comment on column USER.CREATED_AT is '创建时间';
+
+comment on column USER.UPDATE_AT is '修改时间';
+
+comment on column USER.STATUS is '状态，1正常，-1删除';
+
+
