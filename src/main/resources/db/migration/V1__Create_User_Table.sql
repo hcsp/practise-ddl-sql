6ac091a52bab39e911f6d8b193211550a51e77af
Your Name <you@example.com>
2020-12-23 16:18:38 +0800

    fix

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,13 +6,12 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
-Create table user
-(
+create table USER(
     ID bigint primary key auto_increment,
-    NAME varchar(100) NOT NULL,
-    TEL varchar(20) not null unique,
-    ADDRESS varchar (100),
-    CREATED_AT timestamp not null,
-    UPDATED_AT timestamp not null,
-    STATUS TINYINT not null
+    NAME varchar(100) not null ,
+    TEL varchar(20) not null unique ,
+    ADDRESS varchar(100) ,
+    CREATED_AT timestamp not null ,
+    UPDATED_AT timestamp not null ,
+    STATUS tinyint not null
 )
--- src/main/resources/db/migration/V7__Delete_Test_Order.sql
+++ src/main/resources/db/migration/V7__Delete_Test_Order.sql
@@ -1,2 +1,2 @@
 -- 请在这里编写一条SQL语句，将ID为1的订单进行"逻辑删除"
-UPDATE "ORDER" SET STATUS = 0  WHERE ID=1;
+update `ORDER` set STATUS=-1 where ID=1;
