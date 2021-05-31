daa1c332293c8ba1bffaeeb3987eab440111c93d
AndrewZ <zeq_96@163.com>
2021-05-31 17:09:05 +0800

    DONE

--- src/main/resources/db/migration/V1__Create_User_Table.sql
+++ src/main/resources/db/migration/V1__Create_User_Table.sql
@@ -6,3 +6,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table `USER`(
+    ID BIGINT AUTO_INCREMENT COMMENT 'id',
+    NAME VARCHAR(100) NOT NULL COMMENT '用户名',
+    TEL VARCHAR(20) NOT NULL UNIQUE COMMENT '电话',
+    ADDRESS VARCHAR(100) COMMENT '地址',
+    CREATED_AT TIMESTAMP NOT NULL default now() COMMENT '创建时间',
+    UPDATED_AT TIMESTAMP NOT NULL default now() COMMENT '修改时间',
+    STATUS TINYINT NOT NULL COMMENT '状态，1正常，-1删除',
+    PRIMARY KEY (ID)
+);
