fb9c82abe00017a619d9d56f97c6d35562f89237
“idpeng” <630606938@qq.com>
2021-09-23 09:43:58 +0800

    语句联系

--- src/main/resources/db/migration/V4__Insert_Test_User.sql
+++ src/main/resources/db/migration/V4__Insert_Test_User.sql
@@ -6,3 +6,7 @@
 -- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
+
+insert into user
+    (id, name, tel, address, created_at, UPDATED_AT, status)
+values (100, 'TEST_USER', '1234567890', NULL, NOW(), NOW(), 1)
