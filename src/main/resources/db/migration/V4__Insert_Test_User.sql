d06ab8344c00a0c6e163aae1eed6ec2facb42959
soohea <77014206+soohea@users.noreply.github.com>
2022-01-29 23:49:12 +0800

    据库实战：表结构设计、建表语句、insert/update语句练习 (#170)
    据库实战：表结构设计、建表语句、insert/update语句练习 (#170)

--- src/main/resources/db/migration/V4__Insert_Test_User.sql
+++ src/main/resources/db/migration/V4__Insert_Test_User.sql
@@ -6,3 +6,5 @@
 -- CREATED_AT   NOW()
 -- UPDATED_AT   NOW()
 -- STATUS       1
+insert into USER(ID, NAME, TEL, ADDRESS, STATUS, CREATED_AT, UPDATED_AT)
+values (100, 'TEST_USER', '1234567890', null, 1, now(), now())
