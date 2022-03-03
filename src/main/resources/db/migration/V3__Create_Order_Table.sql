a297a66251fa261b2fd2faa52b3a9162ffcf77aa
freetie <freetie@qq.com>
2022-03-03 23:14:49 +0800

    finish sql commands

--- src/main/resources/db/migration/V3__Create_Order_Table.sql
+++ src/main/resources/db/migration/V3__Create_Order_Table.sql
@@ -7,3 +7,13 @@
 -- CREATED_AT   TIMESTAMP    创建时间           不可为空
 -- UPDATED_AT   TIMESTAMP    修改时间           不可为空
 -- STATUS       TINYINT      状态，1正常，-1删除  不可为空
+create table `ORDER` (
+    `ID` bigint primary key auto_increment,
+    `USER_ID` bigint not null,
+    `GOODS_ID` bigint not null,
+    `GOODS_NUM` int not null,
+    `GOODS_PRICE` decimal not null,
+    `CREATED_AT` timestamp not null,
+    `UPDATED_AT` timestamp not null,
+    `STATUS` tinyint not null
+);
